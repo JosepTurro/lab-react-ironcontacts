b475956245f66230f0d1344e025f5fbcdd8ce1ca
Josep Turro <jturrocasanovas@gmail.com>
2022-10-31 09:22:44 +0100

    lab doned sense reserch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import contactsData from "./contacts.json";
+import { useState } from "react";
 
+ 
 function App() {
+  let contacts= contactsData.slice(0,5);
+  const contactesFinals = contactsData.slice(6,contactsData.length);
+  const randomContact = contactesFinals[Math.floor(Math.random()*contactesFinals.length)]
+  const [contactes, setContactes]= useState(contacts);
+  console.log("Actors aaa: ", contacts)
+  
+
+  function addContact(){
+    const contactMood = [...contactes, randomContact ]
+    setContactes (contactMood);
+    console.log("randommmmmm: ",randomContact);
+  }
+
+  function deleteContact(contactId){
+    const contactArray = contactes.filter(contct=>{
+      console.log( "aaaaaa",contct)
+      return contct.id !==  contactId
+      
+    })
+    setContactes(contactArray)
+  }
+  
+  console.log(contacts)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Iron Contacts</h1>
+      <button onClick={addContact}>Add a Contact</button>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+          <th>Actions</th>
+        </tr>
+        {contactes.map(actor=>{
+          return (
+            <tr>
+            <td><img src={actor.pictureUrl} alt='foto actor/a' height='180px'></img></td>
+            <td>{actor.name}</td>
+            <td>{actor.popularity}</td>
+            <td>{(actor.wonOscar=== true)?<span>🏆</span>:""}</td>
+            <td>{(actor.wonEmmy=== true)?<span>🏆</span>:""}</td>
+            <td><button onClick={() => deleteContact(actor.id)}>Delete</button></td>
+          </tr>
+          );
+        })}
+      </table>
+      
     </div>
   );
 }
